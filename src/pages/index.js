81bafc841d97e33ca7a8cf743d5f1438a4c65b83
sdelane2 <seanmartindelaney@gmail.com>
2022-02-03 17:09:01 -0500

    trying this again

--- src/pages/index.js
+++ src/pages/index.js
@@ -141,9 +141,7 @@ const IndexPage = () => {
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
+        
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
